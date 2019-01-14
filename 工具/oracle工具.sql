561a7dcf8368a5f4d2b050cdcd646fe3e5a58928
shenyao <shenyao@ysstech.com>
2019-01-14 20:10:16 +0800

    增加内容

--- 工具/oracle工具.sql
+++ 工具/oracle工具.sql
@@ -1,3 +1,5 @@
+--常规
+
 --查询死锁的语句
 select sess.sid,sess.serial#,lo.oracle_username,lo.os_user_name,ao.object_name,lo.locked_mode  from v$locked_object lo,dba_objects ao,v$session sess where ao.object_id = lo.object_id and lo.session_id = sess.sid;
 --解锁的语句
@@ -25,11 +27,94 @@ impdp BBZX130_CS/1@orcl directory=DUMP_DIR dumpfile=BBZX130.DMP REMAP_SCHEMA=BBZ
 
 expdp BBZX150/1@orcl schemas=BBZX150 directory=DUMP_DIR dumpfile =BBZX156.dmp logfile=BBZX156.log ;
 
+--查看某个表近期的操作情况
+select SQL_TEXT,LAST_ACTIVE_TIME from v$sqlarea where LAST_ACTIVE_TIME >to_date('20140917 10:00:00','yyyymmdd hh24:mi:ss') and upper(SQL_TEXT) like '%T_PORT_INDEX%';
 
+
+--恢复表的数据使用如下方法( 删除数据之后表结构没有发生变化)
+
+alter  table tc_report_index enable row  movement    --开启行移动功能
+
+ 
+
+flashback table tc_report_index to timestamp to_timestamp('2019-01-14 14:00:00','yyyy-mm-dd hh24:mi:ss')  --恢复数据到删除时间点之前的状态
+
+ 
+
+Alter table tc_report_index disable row movement      --关闭行移动功能(一定不能忘记)
+
+
+---使用如下方法( 删除数据之后表结构发生了变化)
+select * from tc_report_index as of timestamp to_timestamp('2019-01-14 19:17:00','yyyy-mm-dd hh24:mi:ss')  --找出被删除的数据
+
+ 
+
+insert into tc_report_index (select * from tc_report_index as of timestamp to_timestamp('2019-01-14 19:17:00','yyyy-mm-dd hh24:mi:ss'));--把删除的数据重新插回原表，但注意主键不要重复
+
+
+--误删整个表
+
+select object_name,original_name,partition_name,type,ts_name,createtime,droptime from recyclebin;
+
+flashback table 表名 to before DROP;
+
+
+--根据SCN恢复表的数据
+
+select current_scn from v$database; --1、获得当前数据库的scn号   切换到sys用户或system用户查询
+
+
+
+select * from 表名 as of scn 1499220; --2、查询当前scn号之前的scn(确定删除的数据是否存在，如果存在，则恢复数据；如果不是，则继续缩小scn号)
+
+
+
+flashback table 表名 to scn 1499220;  --　　3、恢复删除且已提交的数据
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+---优化
 
 --查询SGA各个区域大小的SQL
 
- select pool,sum(bytes)/1024/1024||'MB' from v$sgastat where pool is not null group by pool;
+select pool,sum(bytes)/1024/1024||'MB' from v$sgastat where pool is not null group by pool;
  
+--考虑是否存在过多的reloads和invalidations
+SELECT NAMESPACE,GETS,GETHITS,round(GETHITRATIO*100,2) gethit_ratio,PINS,PINHITS,round(PINHITRATIO*100,2) pinhit_ratio,RELOADS,INVALIDATIONS FROM V$LIBRARYCACHE;
+
+--库缓存的命中率应保持在95%，否则应考虑增大shared_pool_size
+SELECT SUM(pins) "Executions",SUM(reloads) "CacheMisses while Executing",ROUND((SUM(pins)/(SUM(reloads)+SUM(pins)))*100,2) AS "HitRatio Ratio, %" FROM V$LIBRARYCACHE;
  
- 
+--查看共享池可用空间，当sharedpool有过多的可用空间，再调大shared pool则意义不大???????
+
+SELECT pool,name,bytes/1024/1024 FROM v$sgastat WHERE name LIKE '%free memory%' AND pool='shared pool';
+
+--根据硬解析排序，查询硬解析最多的SQL
+select sql_text,SQL_FULLTEXT,parse_calls,executions,loads from v$sqlarea order by loads DESC;
+
+--刷新共享池
+alter system flush shared_pool;
+
+--刷新内存中的数据，重新从硬盘中读取(set autotrace on可以查看是否发生了物理读)
+alter system flush buffer_cache;
+
+
+
+
